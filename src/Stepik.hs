7c7452f6c8a7287270644405db0c0b879bdb1e6b
Egor <bronnikov.40@mail.ru>
2021-07-30 00:57:13 +0300

    Refactor in `Stepik.hs` function `integration`

--- src/Stepik.hs
+++ src/Stepik.hs
@@ -132,12 +132,12 @@ integration :: (Double -> Double) -> Double -> Double -> Double
 integration f a b = let
                         xs = [a + j * h | j <- [0..1000]]
                         h = (b - a) / 1000
-                    in sum [((f (xs !! i) + f (xs !! (i + 1))) / 2) * ((xs !! (i + 1)) - (xs !! i)) | i <- [0..999]]
+                    in sum [( ( f (xs !! i) + f (xs !! (i + 1)) ) / 2 ) * ( xs !! (i + 1) - xs !! i ) | i <- [0..999]]
 
 
 integration' :: (Double -> Double) -> Double -> Double -> Double
-integration' f a b = h * ( (f a + f b)/2 + (sum $ map f xs) )
+integration' f a b = h * ( (f a + f b) / 2 + (sum $ map f xs) )
     where
         n = 1000
-        h = (b-a)/n
-        xs = [a+h*x | x <- [0..n-1]]
+        h = (b - a) / n
+        xs = [a + h * x | x <- [0..n-1]]
