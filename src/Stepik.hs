1326a4a3c1b36b9d7343ecd5184d776b86645461
Egor <bronnikov.40@mail.ru>
2021-08-24 01:20:16 +0300

    Added quick sort

--- src/Stepik.hs
+++ src/Stepik.hs
@@ -475,3 +475,16 @@ instance Show Color where
 change :: (Ord a, Num a) => a -> [[a]]
 change 0 = [[]]
 change amount = [c:cs |c <- coins, amount >= c, cs <- change (amount - c) ]
+
+
+{- Task -}
+data Point = Point Double Double
+
+origin :: Point
+origin = Point 0.0 0.0
+
+distanceToOrigin :: Point -> Double
+distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
+
+distance :: Point -> Point -> Double
+distance (Point x1 y1) (Point x2 y2)= sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
