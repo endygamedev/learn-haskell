44ec5af53536c51a41b985db499466fd67726080
Egor <bronnikov.40@mail.ru>
2021-08-24 01:25:19 +0300

    Edited quick sort

--- src/qsort/QSort.hs
+++ src/qsort/QSort.hs
@@ -7,9 +7,9 @@ qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:[]) = [x]
 qsort (x:xs) = qsort less ++ [x] ++ qsort greater
-	where
-		greater = [g | g <- xs,  g >= x]
-		less = [l | l <- xs, l < x]
+    where
+        greater = [g | g <- xs,  g >= x]
+        less = [l | l <- xs, l < x]
 
 
 main = putStrLn . show $ qsort [10000,9999..1] 
